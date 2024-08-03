a71d7f2bc78c5b8d8fc924ac3894ce71783a5b99
amalfi33 <amalfiamalalena1210@gmail.com>
2024-08-03 10:28:11 +0500

    bootstrap

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Friend(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE ,related_name='user_friend', verbose_name='Пользователь')
+    friend = models.ForeignKey(User , on_delete=models.CASCADE, verbose_name='Друг', related_name='firend')
--- app/views.py
+++ app/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render 
+from django.shortcuts import render , redirect
+from .forms import RegisterForm
+from django.contrib.auth import login , authenticate , logout
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def register(request):
-    
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user=user)
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {"form" : form})
+
+def login_site(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request , username=username,password=password)
+        if user is not None:
+            login(request, user=user)
+            return redirect('index')
+        else:
+            return render(request, 'index.html' , {"error" : "неверное имя или пароль"})
+    return render(request, "login.html")
+
+def logout_site(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('index')
