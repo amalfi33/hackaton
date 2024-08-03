5d7686ee2d94ca384c13dbab80aa2a11b5206900
amalfi33 <amalfiamalalena1210@gmail.com>
2024-08-03 10:55:29 +0500

    views+model

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-
+from .models import Friend, Message, Chat, GroupChat
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -6,9 +6,24 @@ from django.contrib.auth.forms import UserCreationForm
 class RegisterForm(UserCreationForm):   
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username','email', 'password1', 'password2']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
         }
+
+class GroupChatForm(forms.ModelForm):
+    class Meta:
+        model = GroupChat
+        fields = ['name', 'participants']
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['content']
+
+class FriendRequestForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['friend']
--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render , redirect
-from .forms import RegisterForm
+from django.shortcuts import render , redirect,get_object_or_404
+from .forms import RegisterForm ,FriendRequestForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login , authenticate , logout
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
 def index(request):
     return render(request, 'index.html')
 
+# Регистрация и аутентификация 
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -32,3 +35,37 @@ def logout_site(request):
     if request.user.is_authenticated:
         logout(request)
     return redirect('index')
+# ------------------------------------------------------
+
+# Добавление в друзья
+
+
+@login_required
+def send_friend_request(request):
+    if request.method == 'POST':
+        form = FriendRequestForm(request.POST)
+        if form.is_valid():
+            friend_request = form.save(commit=False)
+            friend_request.user = request.user
+            friend_request.save()
+            return redirect('friends_list')
+    else:
+        form = FriendRequestForm()
+    return render(request, 'messenger/send_friend_request.html', {'form': form})
+
+@login_required
+def friends_list(request):
+    friends = Friend.objects.filter(user=request.user, status='accepted')
+    friend_requests = Friend.objects.filter(friend=request.user, status='pending')
+    return render(request, 'messenger/friends_list.html', {'friends': friends, 'friend_requests': friend_requests})
+
+@login_required
+def handle_friend_request(request, friend_request_id, action):
+    friend_request = get_object_or_404(Friend, id=friend_request_id)
+    if action == 'accept':
+        friend_request.status = 'accepted'
+        Friend.objects.create(user=friend_request.friend, friend=friend_request.user, status='accepted')
+    elif action == 'reject':
+        friend_request.status = 'rejected'
+    friend_request.save()
+    return redirect('friends_list')
